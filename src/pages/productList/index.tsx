e6b59f3c8f6a1b774926079188ed1d558e278e3f
lyutaolue <lyutaolue@shgtheatre.com>
2021-06-22 22:49:23 +0800

    feat(productList): add filter for bodyType

--- src/pages/productList/index.tsx
+++ src/pages/productList/index.tsx
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
 import styled from 'styled-components'
 import Swiper from '../../components/Swiper'
-const Wrapper = styled.section`
 
+const { SelectInput } = require('vcc-ui')
+const Wrapper = styled.section`
+    .pl-filter{
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+        padding: 10px;
+    }
+    #vcc-ui-select-input-1{
+        margin-left: 10px;
+        width: 200px;
+    }
 `;
+export type productListType = {
+    id: string,
+    bodyType: string,
+    modelName: string,
+    modelType: string,
+    imageUrl: string,
+}
 const ProductionList = () => {
+    const [initProducts, setInitProducts] = useState([]);
     const [products, setProducts] = useState([]);
     const [slidesPerView, setSlidesPerView] = useState(4);
     const [isPc, setIsPc] = useState(false);
+    const [filterValue, setFilterValue] = useState(0);
+    const [bodyTypeList, setBodyTypeList] = useState([]);
     useEffect(() => {
         const res = require('../../assets/api/cars.json');
+        setInitProducts(res);
+        console.log(res);
+        const typeList: Array<any> = ['all'];
+        res.map((r: productListType) => {
+            if (!typeList.includes(r.bodyType)) {
+                typeList.push(r.bodyType);
+            }
+        })
+        setBodyTypeList(typeList);
         setProducts(res);
     }, [])
     useEffect(() => {
@@ -33,8 +63,22 @@ const ProductionList = () => {
 
         }
     }, [])
+    const filterOnChange = (e) => {
+        const value = e.target.value;
+        setFilterValue(value);
+        setProducts(value === 'all' ? initProducts : initProducts.filter((i: productListType) => i.bodyType === value))
+    }
     return (
         <Wrapper>
+            <div className="pl-filter">
+                <span>BodyType: </span><SelectInput value={filterValue} onChange={e => filterOnChange(e)}>
+                    {
+                        bodyTypeList.map((b: string) => {
+                            return (<option value={b}>{b}</option>)
+                        })
+                    }
+                </SelectInput>
+            </div>
             <Swiper products={products} isPc={isPc} slidesPerView={slidesPerView} />
         </Wrapper>
 
